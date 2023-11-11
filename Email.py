ea8845eb15660c016ac2fa590ac5bb23b2441208
Nguyen Thanh Nhan <ntnhan221@clc.fitus.edu.vn>
2023-11-11 11:19:01 +0700

    still doesnt work

--- Email.py
+++ Email.py
@@ -1,6 +1,12 @@
 import random
 import string
 import time
+import os
+import mimetypes
+import base64
+import re
+
+LINE_LENGTH = 76
 
 class MyMIME:
     Headers = ''
@@ -13,13 +19,11 @@ class MyMIME:
         self.Headers = f'{Content_type}; {charset}\r\n{Content_transfer_encoding}\r\n'
         
     def CreateHeader(self, mime_type : str, file_name : str):
-        Content_type = f'Content-Type: {mime_type}'
+        Content_type = f'Content-Type: {mime_type}; name="{file_name}"'
         Content_transfer_encoding = 'Content-Transfer-Encoding: base64'
         Content_disposition = 'Content-Disposition: attachment'
-        self.Headers = f'{Content_type}\r\n{Content_transfer_encoding}\r\n{Content_disposition}; filename="{file_name}"\r\nMIME-Version: 1.0\r\n'
-        
-    def As_String(self) -> str :
-        return f'{self.Headers}\r\n{self.Content}\r\n'
+        self.Headers = f'{Content_type}\r\n{Content_transfer_encoding}\r\n{Content_disposition}; filename="{file_name}"\r\n'
+    
     
 class Email:
     MIME_Version = 'MIME-Version: 1.0'
@@ -29,40 +33,99 @@ class Email:
     Bcc = ''
     MIME_Parts = []
     
+    def Input(self): 
+        print("Enter email's detail, press enter to skip")
+        self.To = input('To: ') 
+        self.Cc = input('Cc: ')    
+        self.Bcc = input('Bcc: ')    
+        self.Subject = input('Subject: ')
+        
+        print(f'Body (each line should not exceed {LINE_LENGTH} letters):')
+        self.MIME_Parts.append(MyMIME())
+        self.MIME_Parts[0].TextBody()
+        
+        while True:
+            line = input()
+            if (line == ''):
+                break
+            self.MIME_Parts[0].Content += line[:76]
+            self.MIME_Parts[0].Content += '\r\n'
+    
+        if (input('Do you want to send attachment (Y/N): ') == 'Y'):
+            while True:
+                input_path = input('Enter path: ')
+                if (os.path.exists(input_path) and os.path.isfile(input_path)):
+                    if (os.path.getsize(input_path) > 3e+6):
+                        print('Your file\'s size should not exceed 3MB')
+                        continue
+                    
+                    mime_type = mimetypes.guess_type(input_path, strict=True)
+                    file_name = os.path.basename(input_path)
+                    
+                    with open(input_path, 'rb') as fi:
+                        data = fi.read()
+                        data = base64.b64encode(data)
+                        attachment = MyMIME()
+                        attachment.CreateHeader(mime_type[0], file_name)
+                        attachment.Content = str(data)[2:-1]
+                        attachment.Content += '\r\n'
+                        self.MIME_Parts.append(attachment)
+                        
+                    if (input('Do you want to attach another file (Y/N): ') != 'Y'):
+                        break
+                
+                else:
+                    print('You entered an invalid path!')
+                
     def As_String(self, sender_mail: str, sender_name: str) -> str:
         result = ''
+        
         if (len(self.MIME_Parts) > 1):
             self.Boundary = GenerateBoundary()
-            result = f'Content-Type: multipart/mixed; boundary="{self.Boundary}"\r\n'
-        
+            result += (f'Content-Type: multipart/mixed; boundary="{self.Boundary}"\r\n')
+
         #header parts
         current_time = time.time()
         current_date = time.ctime(current_time)
-        result += f'Date: {current_date}\r\n'
+        result += (f'Date: {current_date}\r\n')
         
-        result += f'From: {sender_name} <{sender_name}>\r\n'
+        result += (self.MIME_Version + '\r\n')
+        
+        result += (f'From: {sender_name} <{sender_mail}>\r\n')
         
         if (self.To != ''):
-            result += f'To: {self.To}\r\n'
+            result += (f'To: {self.To}\r\n')
         
         if (self.Cc != ''):
-            result += f'Cc: {self.Cc}\r\n'
+            result += (f'Cc: {self.Cc}\r\n')
             
-        result += f'Subject: {self.Subject}\r\n\r\n'
+        result += (f'Subject: {self.Subject}\r\n')
         
-        #body
+        #body parts
         if (len(self.MIME_Parts) == 1):
-            result += self.MIME_Parts[0].Content
-        else:
-            result += 'This is a multi-part message in MIME format.\r\n'
-            for i in self.MIME_Parts:
-                result += self.Boundary + '\r\n'
-                result += i.As_String()
-            result += self.Boundary + '--\r\n'
+            result += (self.MIME_Parts[0].Headers + '\r\n')
+            for i in re.split('\r\n',self.MIME_Parts[0].Content):
+                if (i != '') :
+                    result += (i + '\r\n')
+        else:         
+            result += ('\r\n' + self.Boundary + '\r\n')
+            result += (self.MIME_Parts[0].Headers + '\r\n')
+            for i in re.split('\r\n',self.MIME_Parts[0].Content):
+                result += (i + '\r\n')
+            
+            for i in range(1, len(self.MIME_Parts)):
+                result += (self.Boundary + '\r\n')
+                result += (self.MIME_Parts[i].Headers + '\r\n')
+                data = self.MIME_Parts[i].Content
+                while (len(data) > 0):
+                    result += (data[:LINE_LENGTH] + '\r\n')
+                    data = data[LINE_LENGTH:]
+                
+            result += (self.Boundary + '--\r\n')
         
-        result += '\r\n.\r\nQUIT\r\n'
+        result += ('\r\n.\r\nQUIT\r\n')
         return result
-    
+       
 def GenerateBoundary() -> str:
     characters = string.ascii_letters + string.digits
     boundary = ''.join(random.choice(characters) for i in range(36))
--- clientSMPT.py
+++ clientSMPT.py
@@ -1,71 +1,25 @@
 import socket
-import time
 import Email
 import re
-import os
-import mimetypes
 import base64
 
 def SendMail():
     HOST = '127.0.0.1'
     PORT = 9696
     
-    user = 'ntnhan@gmail.com'
-    name_of_user = "Nguyễn Thành Nhân"
-    name_of_user = name_of_user.encode('utf-8')
-    name_of_user = base64.b64encode(name_of_user).decode('utf-8')
-    name_of_user = f'=?UTF-8?B?{name_of_user}?='
+    usermail = 'ntnhan@gmail.com'
+    username = "Nguyễn Thành Nhân"
+    username = username.encode('utf-8')
+    username = base64.b64encode(username).decode('utf-8')
+    username = f'=?UTF-8?B?{username}?='
 
     email = Email.Email()
     
     #input Email content
-    print("Enter email's detail, press enter to skip")
-    email.To = input('To: ')
-    
-    email.Cc = input('Cc: ')
-        
-    email.Bcc = input('Bcc: ')
-        
-    email.Subject = input('Subject: ')
-    
-    print('Body:')
-    email.MIME_Parts.append(Email.MyMIME())
-    email.MIME_Parts[0].TextBody()
-    
-    while True:
-        line = input()
-        if (line == ''):
-            break
-        email.MIME_Parts[0].Content += line
-        email.MIME_Parts[0].Content += '\r\n'
-        
-    if (input('Do you want to send attachment (Y/N): ') == 'Y'):
-        while True:
-            input_path = input('Enter path: ')
-            if (os.path.exists(input_path) and os.path.isfile(input_path)):
-                if (os.path.getsize(input_path) > 3e+6):
-                    print('Your file\'s size should not exceed 3MB')
-                    continue
-                
-                mime_type = mimetypes.guess_type(input_path, strict=True)
-                file_name = os.path.basename(input_path)
-                
-                with open(input_path, 'rb') as fi:
-                    data = fi.read()
-                    data = base64.b64encode(data)
-                    attachment = Email.MyMIME()
-                    attachment.CreateHeader(mime_type[0], file_name)
-                    attachment.Content = str(data)[2:-1]
-                    attachment.Content += '\r\n'
-                    email.MIME_Parts.append(attachment)
-                    
-                if (input('Do you want to attach another file (Y/N): ') != 'Y'):
-                    break
+    email.Input()
             
-            else:
-                print('You entered an invalid path!')
-            
-    # print(email.As_String(sender_mail=user, sender_name=name_of_user))
+    # li = email.As_List(sender_mail=usermail, sender_name=username)
+    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
         #initialize TCP connection
         client.connect((HOST, PORT))
@@ -79,7 +33,7 @@ def SendMail():
         msg = 'EHLO [127.0.0.1]\r\n'
         client.sendall(msg.encode('utf-8'))
         
-        msg = f'MAIL FROM:<{user}>\r\n'
+        msg = f'MAIL FROM:<{usermail}>\r\n'
         client.sendall(msg.encode('utf-8'))
         
         recipentList = []
@@ -100,7 +54,6 @@ def SendMail():
         msg = 'DATA\r\n'
         client.sendall(msg.encode('utf-8'))
         
-        data = email.As_String(sender_mail=user, sender_name=name_of_user).encode('utf-8')
-        client.sendall(data)
+        client.sendall(email.As_String(sender_mail=usermail, sender_name=username).encode('utf-8'))
         
         client.recv(1024)
