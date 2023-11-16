80b4393a96813fcf58384919c86516b74aaa5103
Nguyen Thanh Nhan <ntnhan221@clc.fitus.edu.vn>
2023-11-16 17:45:15 +0700

    nothing matters

--- adu_GUI.py
+++ adu_GUI.py
@@ -3,6 +3,7 @@ import os
 import mimetypes
 import base64
 import json
+import re
 import socket
 
 import Email
@@ -11,20 +12,20 @@ LINE_LENGTH = 76
 def SendMail():
     def Send():
         #get to, cc, bcc, subject
-        email.To = ent_to.get()
-        email.Cc = ent_cc.get()
-        email.Bcc = ent_bcc.get()
-        email.Subject = ent_subject.get()
+        email.To = ent_to.get().encode('utf-8')
+        email.Cc = ent_cc.get().encode('utf-8')
+        email.Bcc = ent_bcc.get().encode('utf-8')
+        email.Subject = ent_subject.get().encode('utf-8')
         
         #get content
         content = txt_content.get('1.0', tk.END)
         email.MIME_Parts.append(Email.MyMIME())
-        email.MIME_Parts[0].CreateBodyHeader()
+        email.MIME_Parts[0].create_body_headers()
         
         for line in content.split('\n'):
             if (line != ''):
-                email.MIME_Parts[0].Content += line[:LINE_LENGTH]
-                email.MIME_Parts[0].Content += '\r\n'
+                email.MIME_Parts[0].Content += line[:LINE_LENGTH].encode('utf-8')
+                email.MIME_Parts[0].Content += '\r\n'.encode('utf-8')
         
         #get attachment
         for input_path in ent_attachment.get().split(', '):
@@ -36,9 +37,9 @@ def SendMail():
                         data = fi.read()
                         data = base64.b64encode(data)
                         attachment = Email.MyMIME()
-                        attachment.CreateAttachmentHeader(mime_type[0], file_name)
-                        attachment.Content = str(data)[2:-1]
-                        attachment.Content += '\r\n'
+                        attachment.create_attachment_headers(mime_type[0], file_name)
+                        attachment.Content = data
+                        attachment.Content += b'\r\n'
                         email.MIME_Parts.append(attachment)
                     
         #send through socket    
@@ -77,24 +78,27 @@ def SendMail():
             client.sendall(msg.encode('utf-8'))
             
             recipentList = []
-            if (email.To != ''):
-                recipentList += email.To.split(', ')
-            if (email.Cc != ''):
-                recipentList += email.Cc.split(', ')
-            if (email.Bcc != ''):
-                recipentList += email.Bcc.split(', ')
+            if (email.To != b''):
+                recipentList += re.split(b',|, | ', email.To)
+            if (email.Cc != b''):
+                recipentList += re.split(b',|, | ', email.Cc)
+            if (email.Bcc != b''):
+                recipentList += re.split(b',|, | ', email.Bcc)
                 
             for rcpt in list(set(recipentList)):
-                msg = f'RCPT TO:<{rcpt}>\r\n'
-                client.sendall(msg.encode('utf-8'))
-                if (client.recv(1024).decode('utf-8')[:3] != '250'):
-                    raise RuntimeError('250 reply not received from server when sending RCPT')
+                if (rcpt != b''):
+                    msg = b'RCPT TO:<' + rcpt + b'>\r\n'
+                    client.sendall(msg)
+                    response = client.recv(1024).decode('utf-8')
+                    if (response[:3] != '250'):
+                        raise RuntimeError('Error sending RCPT')
             
             #send mail content
             msg = 'DATA\r\n'
             client.sendall(msg.encode('utf-8'))
             
-            client.sendall(email.As_String(sender_mail=usermail, sender_name=username).encode('utf-8'))
+            msg = email.As_Byte()
+            client.sendall(msg)
             
             msg = '\r\n.\r\nQUIT\r\n'
             client.sendall(msg.encode('utf-8'))
