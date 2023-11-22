07a2fce47169c5e22452c4880f66335e28504b14
Nguyen Thanh Nhan <ntnhan221@clc.fitus.edu.vn>
2023-11-22 22:35:20 +0700

    added autoload interval with threading

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import clientSMTP
 import clientPOP3
+import threading
+import time
+import json
 
 def main():
+    def auto_load():
+        with open('config.json', 'r') as fin:
+            config = json.load(fin)
+            interval = config['General']['Autoload']
+        
+        while True:
+            get_message_thread = threading.Thread(target=clientPOP3.GetMessage)
+            get_message_thread.start()
+            get_message_thread.join()
+            time.sleep(interval)
+            
+    auto_load_thread = threading.Thread(target=auto_load, daemon=True)
+    auto_load_thread.start()
+    
     while True:
         print("Options:")
         print("1. Send email")
